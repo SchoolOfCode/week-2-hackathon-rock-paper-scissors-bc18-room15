8f5bdda07918d4194c1204b44059fb76a937a756
Timothy Li <timothy93li@gmail.com>
2024-12-05 14:22:13 +0000

    task 0, 1, 2 complete. task 3 in progress, prompt working, result gives undefined

--- script.js
+++ script.js
@@ -1,30 +1,54 @@
 // create a let variable called playerMove that stores a move of their choice (rock, paper, scissors)✅
 //create a let variable called computermove that stores either rock, paper or scissors. Use math.random()✅
-// create a if else loop to determine winner
-//hard-code the variables to check if its working. Use console.log
-// Create a function - The variables from before should now be taken in as parameters so that you can call the function with any two moves
+// create a if else loop to determine winner ✅
+//hard-code the variables to check if its working. Use console.log ✅
+// Create a function - The variables from before should now be taken in as parameters so that you can call the function with any two moves ✅ 
 // Using `prompt`, get a user-inputted value for the player move.
 // Then call your function with that value as the player move and the hard-coded computer move. Display the result using `alert`.
 
-let playerMove = "rock";
-let computerMove = "rock";
+// let playerMove = "rock";
 
-if (playerMove === "rock" && computerMove === "rock") {
-         console.log("It's a draw!");
-}   else if (playerMove === "rock" && computerMove === "paper") {
-         console.log("Computer wins!.");
-}   else if (playerMove === "rock" && computerMove === "rock"); {
-        console.log("Player wins!");
-}   else if (playerMove === "paper" && computerMove === "rock"); {
-        console.log("Player wins!");
-}   else if (playerMove === "paper" && computerMove === "paper"); {
-        console.log("It's a draw!");
-}   else if (playerMove === "paper" && computerMove === "scissors"); {
-        console.log("Computer wins!.");
-}   else if (playerMove === "scissors" && computerMove === "rock"); {
-        console.log("Computer wins!.");
-}   else if (playerMove === "scissors" && computerMove === "paper"); {
-        console.log("Player wins!");
-}   else if (playerMove === "scissors" && computerMove === "scissors"); {
-        console.log("Player wins!");
-}   
+
+// if (playerMove === computerMove) {
+//          console.log("It's a draw!");
+// }   else if (playerMove === "rock" && computerMove === "paper") {
+//          console.log("Computer wins!");
+// }   else if (playerMove === "rock" && computerMove === "scissors") {
+//                 console.log("Player wins!");
+// }   else if (playerMove === "paper" && computerMove === "rock") {
+//         console.log("Player wins!");
+// }   else if (playerMove === "paper" && computerMove === "scissors") {
+//         console.log("Computer wins!");
+// }   else if (playerMove === "scissors" && computerMove === "rock") {
+//         console.log("Computer wins!");
+// }   else if (playerMove === "scissors" && computerMove === "paper") {
+//         console.log("Player wins!");
+// }
+
+// let playerMove = prompt(`Type "rock", "paper" or "scissors"`); assigning new variable to store user prompt
+// let playerInput = prompt(`Type "rock", "paper" or "scissors"`);
+// let computerMove = "scissors";
+
+function getWinner(playerMove, computerMove) {
+
+        if (playerMove === computerMove) {
+                return 0;
+       }   else if (playerMove === "rock" && computerMove === "paper") {
+                return -1;
+       }   else if (playerMove === "rock" && computerMove === "scissors") {
+                return 1;
+       }   else if (playerMove === "paper" && computerMove === "rock") {
+                return 1;
+       }   else if (playerMove === "paper" && computerMove === "scissors") {
+                return -1;
+       }   else if (playerMove === "scissors" && computerMove === "rock") {
+                return -1;
+       }   else if (playerMove === "scissors" && computerMove === "paper") {
+                return 1;
+       }     
+}
+let playerInput = prompt(`Type "rock", "paper" or "scissors"`);
+let result = getWinner(playerInput, "scissors");
+
+
+// let result = getWinner("rock", "paper")
