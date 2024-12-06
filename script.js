0c5ada6c67e4bd87c61a037ebfcdd1bde123f70e
Timothy Li <timothy93li@gmail.com>
2024-12-06 10:51:51 +0000

    task 5 done

--- script.js
+++ script.js
@@ -5,9 +5,16 @@
 // Create a function - The variables from before should now be taken in as parameters so that you can call the function with any two moves ✅ 
 // Using `prompt`, get a user-inputted value for the player move.✅
 // Then call your function with that value as the player move and the hard-coded computer move. Display the result using `alert`. ✅
-// Ensure math.random() generates into randow rock, paper, scissors and not literal numbers. 
-// Create an array which stores 3 values (rock, paper and scissors). Console.log
-// use math.random to access an index of that array.
+// Ensure math.random() generates into randow rock, paper, scissors and not literal numbers. ✅
+// Create an array which stores 3 values (rock, paper and scissors). Console.log ✅
+// use math.random to access an index of that array. ✅
+// Create a function to call in the console that runs the game, instead of the prompt appearing immediately ✅
+// Create a while loop and confirm to play the game indefinitely until the player wants to stop ✅
+        //confirm what player wants to do ✅
+        //put result of confirm into a variable ✅
+        //while loop that checks if variable is true or false ✅
+// Whilst testing the game, noticed player input only accepts exact match to condition 
+        //
 
 // let playerMove = "rock";
 
@@ -32,6 +39,8 @@
 // let playerInput = prompt(`Type "rock", "paper" or "scissors"`);
 // let computerMove = "scissors";
 
+
+
 function getWinner(playerMove, computerMove) {
 
         if (playerMove === computerMove) {
@@ -50,18 +59,17 @@ function getWinner(playerMove, computerMove) {
                 return 1;
        }     
 }
-//let playerMove = prompt(`Type "rock", "paper" or "scissors"`);
-//let moves = ["rock", "paper", "scissors"];
-// let randomMove = moves[Math.floor(Math.random() * 3)]
 
-// let result = getWinner(playerMove, randomMove);
+function  playGame() {
 
+let continueGame = true;
 
-function  playGame() {  
- let playerMove = prompt(`Type "rock", "paper" or "scissors"`);
- let moves = ["rock", "paper", "scissors"];
- let randomMove = moves[Math.floor(Math.random() * 3)]
- let result = getWinner(playerMove, randomMove);      
+while (continueGame === true) {
+
+let playerMove = prompt(`Type "rock", "paper" or "scissors"`).toLowerCase();
+let moves = ["rock", "paper", "scissors"];
+let randomMove = moves[Math.floor(Math.random() * 3)];
+let result = getWinner(playerMove, randomMove);      
         
 if (result === 0) {
         alert("It's a draw");
@@ -70,11 +78,6 @@ if (result === 0) {
 } else if (result === -1) {
         alert("Computer wins");
 };
+continueGame = confirm("Would you like to continue?");
 }
-
-// console.log(moves);
-
-
-
-
-// let result = getWinner("rock", "paper")
+}
